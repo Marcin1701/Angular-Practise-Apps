bbbb147ae2b15b545e5c30daab6914bf5acfe0d5
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-26 23:48:03 +0200

    @Input() move data between components ranking component

---
+++ best-quotes/src/app/ranking/ranking.component.ts
@@ -0,0 +1,14 @@
+import { Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { Quotation } from '../models/quotation';
+
+@Component({
+  selector: 'app-ranking',
+  templateUrl: './ranking.component.html',
+  styleUrls: ['./ranking.component.css']
+})
+export class RankingComponent{
+
+  @Input()
+  quotes: Quotation[];
+}
