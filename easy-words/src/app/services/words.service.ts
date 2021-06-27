bbfdc94cd690f21c52affcf9712b42ff7a9ba02c
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-28 01:11:30 +0200

    Service: add new service

---
+++ easy-words/src/app/services/words.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { WORDS } from '../data/data-base';
+import { WordType } from '../data/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WordsService {
+
+  constructor() { }
+}
