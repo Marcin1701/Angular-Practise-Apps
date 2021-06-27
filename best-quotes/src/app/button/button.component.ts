157b60e59a8625e5e63869278a06801d3f3b0800
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-27 10:51:51 +0200

    ng-content - reusable button, element, custom css passed by parameter

---
+++ best-quotes/src/app/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Output } from '@angular/core';
+import { Input } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+  @Output()
+  clickButton = new EventEmitter<void>();
+
+  @Input()
+  customClasses: string;
+
+}
