59b6cf4bfa7e9aac4fd211283851ed763132ec2b
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-28 22:13:32 +0200

    Service: RxJS implemented in question component

--- easy-words/src/app/components/question/question.component.ts
+++ easy-words/src/app/components/question/question.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WordType } from 'src/app/data/models';
 import { WordsService } from 'src/app/services/words.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit, OnDestroy {
 
   word: WordType = null;
 
-  constructor(
-    private wordsService: WordsService
-  ) {}
+  private words = [];
+
+  private subscription: Subscription;
+
+  constructor(private wordsService: WordsService) {}
 
   ngOnInit(): void {
-    this.fetchWord();
+    this.subscription = this.wordsService.getWords().subscribe(
+      (words: WordType[]) => {
+        this.words = words;
+        this.fetchWord();
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   addToNouns(word: WordType): void {
@@ -29,12 +41,7 @@ export class QuestionComponent implements OnInit {
     this.fetchWord();
   }
 
-  check(): void {
-    this.wordsService.check();
-  }
-
   private fetchWord(): void {
-    this.word = 
-      this.wordsService.getWords().shift();
+    this.word = this.words.shift();
   }
 }
