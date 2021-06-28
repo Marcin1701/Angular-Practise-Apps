377af3dd5196f8894e8bb5d6ac1fc5c88b545e93
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-28 22:22:44 +0200

    Service: async pipe, @Input() on setter

--- easy-words/src/app/components/answers/answers.component.ts
+++ easy-words/src/app/components/answers/answers.component.ts
@@ -10,11 +10,19 @@ import { WordType } from 'src/app/data/models';
 export class AnswersComponent implements OnInit {
 
   @Input() title: string;
-  @Input() words: WordType[];
+  words: WordType[] = [];
+
+  @Input() set word(word: WordType) {
+    if (word) {
+      this.words.push(word);
+    }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
