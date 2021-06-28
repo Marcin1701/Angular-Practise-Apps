cd7cf251c05ff8f5c4b1fe109477ed6cfd9a806a
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-28 20:46:17 +0200

    RxJS: use of subscribe

--- rxjs-example/src/app/services/data.service.ts
+++ rxjs-example/src/app/services/data.service.ts
@@ -11,10 +11,28 @@ export class DataService {
 
   subject = new Subject<Person[]>();
 
-  constructor(private dataBaseService: DataBaseService) {}
+  constructor(private dataBaseService: DataBaseService) {
+    this.init();
+  }
+
+  addPerson(person: Person) {
+
+  }
 
   private init(): void {
-    
+    this.dataBaseService.fetchPersons().subscribe(
+      // Recieve person and send into behaviorSubject and subject
+      (persons: Person[]) => {
+        this.behaviorSubject.next(persons);
+        this.subject.next(persons);
+      },
+      // Log any errors
+      error => console.log(error),
+      // Invoke complete method
+      () => console.log('Complete!')
+    );
   }
+
+
 
 }
