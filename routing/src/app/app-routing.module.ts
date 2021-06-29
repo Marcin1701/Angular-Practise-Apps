f4855818b9b4eace26f744141a50765120cbca10
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-30 00:10:27 +0200

    Routing: add another routing and inplement redirect in html

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { MoviesComponent } from './pages/movies/movies.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
+import { MoviesInCategoryComponent } from './pages/categories/movies-in-category/movies-in-category.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path: 'categories',
     component: CategoriesComponent
+  },
+  {
+    path: 'category/:category',
+    component: MoviesInCategoryComponent
   },
   {
     path: '**',
