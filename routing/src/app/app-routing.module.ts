a71e11148f082165809eb6ee7559b50df56f84e0
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-29 22:39:34 +0200

    Routing: add another routing

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 const routes: Routes = [
   {
@@ -14,8 +15,12 @@ const routes: Routes = [
     component: MoviesComponent
   },
   {
+    path: 'categories',
+    component: CategoriesComponent
+  },
+  {
     path: '**',
-    component: PageNotFoundComponent 
+    component: PageNotFoundComponent
   }
 ];
 
