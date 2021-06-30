eab8cb949f50c1deef3b58fda944ff059abd9044
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-30 12:00:30 +0200

    Routing: add years components

---
+++ routing/src/app/pages/years/movie-in-years/movie-in-years.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-in-years',
+  templateUrl: './movie-in-years.component.html',
+  styleUrls: ['./movie-in-years.component.css']
+})
+export class MovieInYearsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
