2c5a5f16a3307ad3130ce8da8e23ba9f2e966f6f
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-30 18:15:29 +0200

    Form: ngSubmit - get data from form

--- http-movies-service/src/app/pages/add-movie/add-movie.component.ts
+++ http-movies-service/src/app/pages/add-movie/add-movie.component.ts
@@ -17,7 +17,7 @@ export class AddMovieComponent implements OnInit {
     this.model = { title: 'Film' };
   }
 
-  send(movieForm: NgForm) {
-    console.log(movieForm);
+  send() {
+    console.log(this.model);
   }
 }
