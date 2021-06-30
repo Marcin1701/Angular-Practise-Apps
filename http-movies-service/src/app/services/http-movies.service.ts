262a5b22e5d97d650dd8b8d4759ec797c96d9f52
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-30 13:21:12 +0200

    Http: GET data from backend

--- http-movies-service/src/app/services/http-movies.service.ts
+++ http-movies-service/src/app/services/http-movies.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Movie } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpMoviesService {
 
-  constructor() { }
+  private url = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.url + '/movies');
+  }
 }
