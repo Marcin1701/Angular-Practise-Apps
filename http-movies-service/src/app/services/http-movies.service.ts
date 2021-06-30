22b66ab124801d61c348bc3f5919c5094945b473
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-30 13:23:19 +0200

    Http: console.log data recieved from backend

--- http-movies-service/src/app/services/http-movies.service.ts
+++ http-movies-service/src/app/services/http-movies.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from '../models/movie';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,7 @@ export class HttpMoviesService {
   constructor(private http: HttpClient) {}
 
   getMovies(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(this.url + '/movies');
+    return this.http.get<Movie[]>(this.url + '/movies')
+      .pipe(tap(console.log));
   }
 }
