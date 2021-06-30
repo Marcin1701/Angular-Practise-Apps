0d2833a3e7fb8a809848f1eb241b4e15ffc5b1ae
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-30 18:17:01 +0200

    Form: add form HTML

--- http-movies-service/src/app/pages/add-movie/add-movie.component.ts
+++ http-movies-service/src/app/pages/add-movie/add-movie.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-movie',
